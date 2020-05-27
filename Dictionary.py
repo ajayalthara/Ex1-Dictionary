483ea18b368acea76ac5ee57cd06a60446a97dbc
Ajay AJ <ajayalthara@gmail.com>
2020-05-26 21:17:02 -0500

    EOD Version

--- Dictionary.py
+++ Dictionary.py
@@ -9,6 +9,9 @@ def searchWord(word):
         if word in datadict:
             for meaning in datadict[word]:
                 print(meaning)
+        elif word.title() in datadict:
+            for meaning in datadict[word.title()]:
+                print(meaning)
         elif len(difflib.get_close_matches(word, datadict.keys(), cutoff=0.5)) > 0:
             suggestion = difflib.get_close_matches(word, datadict.keys(), cutoff=0.5)[0]
             print("The word '{user_word}' doesnt exist. Did you mean '{suggest}'?".format(user_word=word.capitalize(), suggest=suggestion.capitalize()))
@@ -17,11 +20,22 @@ def searchWord(word):
             if user_choice == "Y":
                 for meaning in datadict[suggestion]:
                     print(meaning)
+            elif user_choice == "N":
+                user_choice2 = input("Do you like to enter the word again?? (Y/N): ")
+                user_choice2 = user_choice2.upper()
+                if user_choice2 == "Y":
+                    main_prog()
+                else:
+                    print("Thanks for using the program. Existing")
             else:
-                print("Thank you!!")
+                print("Not a valid input. Exiting. Thank you!!")
         else:
             print("Word doesnt exist. Please re-enter")
 
 
-user_word = input("Enter the word: ")
-searchWord(user_word)
+def main_prog():
+    user_word = input("Enter the word: ")
+    searchWord(user_word)
+
+
+main_prog()
