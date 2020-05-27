a5187657d527bd1489f067879cbc11cc478cb928
Ajay AJ <ajayalthara@gmail.com>
2020-05-27 17:02:45 -0500

    Final Commit

--- Dictionary.py
+++ Dictionary.py
@@ -12,6 +12,9 @@ def searchWord(word):
         elif word.title() in datadict:
             for meaning in datadict[word.title()]:
                 print(meaning)
+        elif word.upper() in datadict:
+            for meaning in datadict[word.upper()]:
+                print(meaning)
         elif len(difflib.get_close_matches(word, datadict.keys(), cutoff=0.5)) > 0:
             suggestion = difflib.get_close_matches(word, datadict.keys(), cutoff=0.5)[0]
             print("The word '{user_word}' doesnt exist. Did you mean '{suggest}'?".format(user_word=word.capitalize(), suggest=suggestion.capitalize()))
@@ -21,7 +24,7 @@ def searchWord(word):
                 for meaning in datadict[suggestion]:
                     print(meaning)
             elif user_choice == "N":
-                user_choice2 = input("Do you like to enter the word again?? (Y/N): ")
+                user_choice2 = input("Do you like to enter the word again?? (Y/N):")
                 user_choice2 = user_choice2.upper()
                 if user_choice2 == "Y":
                     main_prog()
